5221b9528f8715f25dc8f8121e57b2aec6dcaebc
AsapAnt <35341829+Anthonybommarito@users.noreply.github.com>
2019-02-27 12:35:08 -0500

    updated hw 2 to get plots to work in color and output png file

--- hw 2/integ_plot.h
+++ hw 2/integ_plot.h
@@ -3,13 +3,14 @@ set title 'integral error plots'
 set xlabel ' log(N)'
 set ylabel 'log(error)'
 set pointsize 1.5
-set key top left
+set key top right
 set timestamp
 f(x)=m*x+b
 fit [0:1] f(x) 'integ.dat' using 1:2 via m,b
-plot 'integ.dat' using 1:2  linecolor rgb "violet" title 'simpsons' ,'integ1.dat' using 1:2 title 'milne' linecolor rgb "blue"
+plot 'integ.dat' using 1:2  title 'simpsons' ,'integ1.dat' using 1:2 title 'milne'
 g(x)=l*x+k
 fit [0:1.2] g(x) 'integ1.dat' using 1:2 via l,k
-set out 'int_plot.ps'
+set term png
+set output "error.png"
 
 replot
